716202628c5157fca16715333203486e71ffdca4
MarkWLee <lakala@lakaladeMacBook-Pro.local>
2016-12-19 10:21:27 +0800

    hasbdjahsbd

---
+++ jjbbbjbhj/jjbbbjbhj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jjbbbjbhj
+//
+//  Created by lakala on 2016/12/19.
+//  Copyright © 2016年 lakala.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jjbbbjbhj/jjbbbjbhj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jjbbbjbhj
+//
+//  Created by lakala on 2016/12/19.
+//  Copyright © 2016年 lakala.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
